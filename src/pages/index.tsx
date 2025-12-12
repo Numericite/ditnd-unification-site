52b6b7edadc60be99cb1d9c2db6c432a8571741f
SenhaoNum <s.jiang@numericite.eu>
2025-12-12 14:28:56 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,23 @@ import Head from "next/head";
 import { tss } from "tss-react";
 import { fr } from "@codegouvfr/react-dsfr";
 import { PersonaTiles } from "~/components/Accueil/PersonaTiles";
+import { api } from "~/utils/api";
+
+type TDH = {
+  id: number;
+  name: string;
+  description: string;
+  acronym: string;
+  slug: string;
+  updatedAt: string;
+  createdAt: string;
+};
 
 export default function Home() {
   const { classes } = useStyles();
-
+  const { data: conditions, isLoading: isLoadingHomePage } =
+    api.condition.all.useQuery();
+  console.log(conditions);
   const tiles = [
     {
       title: "Je suis une personne concernée",
@@ -30,59 +43,62 @@ export default function Home() {
       <Head>
         <title>DITND - Accueil</title>
       </Head>
-      <div>
-        <div className={fr.cx("fr-pt-4w", "fr-pb-4w")}>
-          <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-            <div className={fr.cx("fr-col-12", "fr-col-lg-6")}>
-              <h1>Autisme et troubles du neuro-développement</h1>
-              <p>
-                La plateforme nationale au services des personnes concernées par
-                un trouble du neurodéveloppement, les parents, et les
-                professionnels.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={fr.cx("fr-pt-4w", "fr-pb-4w")}>
-          <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-            <div className={fr.cx("fr-col-12", "fr-col-lg-12")}>
-              <h2>Qui êtes vous</h2>
-              <div className={fr.cx("fr-text--sm")}>
-                La plateforme nationale au services des personnes concernées par
-                un trouble du neurodéveloppement
-              </div>
-            </div>
-            <div
-              className={fr.cx(
-                "fr-grid-row",
-                "fr-grid-row--gutters",
-                "fr-grid-row--middle"
-              )}
-              style={{
-                width: "100%",
-                alignItems: "stretch",
-                marginLeft: 0,
-                marginRight: 0,
-              }}
-            >
-              {tiles.map((tile, index) => (
-                <PersonaTiles key={index} tile={tile} />
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className={fr.cx("fr-pt-4w", "fr-pb-4w")}>
-          <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-            <div className={fr.cx("fr-col-12", "fr-col-lg-12")}>
-              <h2>Fiches pratiques les plus lues</h2>
-              <div className={fr.cx("fr-text--sm")}>
-                La plateforme nationale au services des personnes concernées par
-                un trouble du neurodéveloppement
+      {isLoadingHomePage ? (
+        <div className="">...Loading</div>
+      ) : (
+        <div>
+          <div className={fr.cx("fr-py-4w")}>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+              <div className={fr.cx("fr-col-12", "fr-col-lg-6")}>
+                <h1>Autisme et troubles du neuro-développement</h1>
+                <p>
+                  La plateforme nationale au services des personnes concernées
+                  par un trouble du neurodéveloppement, les parents, et les
+                  professionnels.
+                </p>
               </div>
             </div>
           </div>
+          <div className={fr.cx("fr-pt-4w", "fr-pb-4w")}>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+              <div className={fr.cx("fr-col-12", "fr-col-lg-12")}>
+                <h2>Qui êtes vous</h2>
+                <div className={fr.cx("fr-text--sm")}>
+                  La plateforme nationale au services des personnes concernées
+                  par un trouble du neurodéveloppement
+                </div>
+              </div>
+              <div
+                className={fr.cx(
+                  "fr-grid-row",
+                  "fr-grid-row--gutters",
+                  "fr-grid-row--middle"
+                )}
+                style={{
+                  width: "100%",
+                  alignItems: "stretch",
+                  marginLeft: 0,
+                }}
+              >
+                {tiles.map((tile, index) => (
+                  <PersonaTiles key={index} tile={tile} />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className={fr.cx("fr-pt-4w", "fr-pb-4w")}>
+            <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+              <div className={fr.cx("fr-col-12", "fr-col-lg-12")}>
+                <h2>Fiches pratiques les plus lues</h2>
+                <div className={fr.cx("fr-text--sm")}>
+                  La plateforme nationale au services des personnes concernées
+                  par un trouble du neurodéveloppement
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
