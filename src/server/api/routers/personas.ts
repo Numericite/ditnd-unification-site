6b45e25203602e36d6570aa0a0b949658a593f93
SenhaoNum <s.jiang@numericite.eu>
2025-12-16 11:36:22 +0100

    Feat : Added persona tRPC router
    Feat : Added persona tRPC router that returns every professional personas

---
+++ src/server/api/routers/personas.ts
@@ -0,0 +1,30 @@
+import { getPayload } from "payload";
+import type { PersonaTile } from "~/components/Accueil/PersonaTiles";
+import payloadConfig from "~/payload/payload.config";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const payload = await getPayload({ config: payloadConfig });
+
+export const personaRouter = createTRPCRouter({
+  professionals: publicProcedure.query(async () => {
+    const result = await payload.find({
+      collection: "personas",
+      where: {
+        slug: {
+          like: "pro%",
+        },
+      },
+      select: {
+        updatedAt: false,
+        createdAt: false,
+      },
+    });
+    const res = result.docs.map(
+      (persona): PersonaTile => ({
+        ...persona,
+        display: "afterProfessionnal",
+      })
+    );
+    return res;
+  }),
+});
