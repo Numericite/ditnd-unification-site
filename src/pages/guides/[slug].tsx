af881e7c1a456d3908822620acaa2703b6ae0fe1
SenhaoNum <s.jiang@numericite.eu>
2026-01-05 16:37:22 +0100

    Feat : added more features into practical guides pages
    Feat : added more features into practical guides pages, added summary, better indents and fixed some issues with the editor css not correctly displaying

--- src/pages/guides/[slug].tsx
+++ src/pages/guides/[slug].tsx
@@ -5,10 +5,31 @@ import { Summary } from "@codegouvfr/react-dsfr/Summary";
 
 import Breadcrumb from "@codegouvfr/react-dsfr/Breadcrumb";
 import Head from "next/head";
+import { tss } from "tss-react";
+import Button from "@codegouvfr/react-dsfr/Button";
+import { type FrIconClassName } from "@codegouvfr/react-dsfr/fr/generatedFromCss/classNames";
+
+const socials: { icon: FrIconClassName; onClick: () => void }[] = [
+  {
+    icon: "fr-icon-facebook-circle-line",
+    onClick: () => {},
+  },
+  { icon: "fr-icon-twitter-x-line", onClick: () => {} },
+  { icon: "fr-icon-linkedin-box-line", onClick: () => {} },
+  { icon: "fr-icon-mail-line", onClick: () => {} },
+  {
+    icon: "fr-icon-links-line",
+    onClick: () => {
+      const url = location.href;
+      navigator.clipboard.writeText(url);
+    },
+  },
+];
 
 export default function PracticalGuidePage() {
   const router = useRouter();
   const slug = router.query.slug as string;
+  const { classes, cx } = useStyles();
 
   const { data: guideData, isLoading: isLoadingData } =
     api.practicalGuide.getBySlug.useQuery({ slug: slug });
@@ -45,8 +66,16 @@ export default function PracticalGuidePage() {
             ]}
           />
           <div className={fr.cx("fr-grid-row")}>
-            <div className={fr.cx("fr-pr-3v")}>
+            <div
+              className={fr.cx(
+                "fr-pr-3v",
+                "fr-col-lg-3",
+                "fr-col-sm-12",
+                "fr-mb-2w"
+              )}
+            >
               <Summary
+                className={cx(classes.summarySticky)}
                 links={[
                   {
                     linkProps: {
@@ -66,19 +95,56 @@ export default function PracticalGuidePage() {
                     },
                     text: "Titre de l’ancre 3",
                   },
+                  {
+                    linkProps: {
+                      href: "#fiches-pratiques",
+                    },
+                    text: "Ces fiches pratiques qui pourraient vous intéresser",
+                  },
+                  {
+                    linkProps: {
+                      href: "#formations",
+                    },
+                    text: "Ces fiches formations qui pourraient vous intéresser",
+                  },
                 ]}
                 title="Sommaire"
               />
             </div>
 
-            <div className={fr.cx("fr-col-12", "fr-col-md-8")}>
+            <div className={fr.cx("fr-col-12", "fr-col-lg-9")}>
               <h4>Fiches pratiques</h4>
               {guide.html && (
                 <div
-                  className="content"
+                  className={cx(classes.wysiwig)}
                   dangerouslySetInnerHTML={{ __html: guide.html }}
                 ></div>
               )}
+              <div className={cx(classes.footerContent)}>
+                <div className={fr.cx("fr-mt-2w")}>
+                  <p className={fr.cx("fr-text--md")}>Partager la page</p>
+                  {socials.map((social) => (
+                    <Button
+                      iconId={social.icon}
+                      onClick={social.onClick}
+                      priority="tertiary"
+                      title="Label button"
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className={cx(classes.footerContent)}>
+                <div className={fr.cx("fr-mt-2w")}>
+                  <h5 id="fiches-pratiques">
+                    Ces fiches pratiques qui pourraient vous intéresser{" "}
+                  </h5>
+                </div>
+                <div className={fr.cx("fr-mt-2w")}>
+                  <h5 id="formations">
+                    Ces fiches formations qui pourraient vous intéresser{" "}
+                  </h5>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -86,3 +152,19 @@ export default function PracticalGuidePage() {
     </>
   );
 }
+
+const useStyles = tss.withName(PracticalGuidePage.name).create(() => ({
+  summarySticky: {
+    position: "sticky",
+    top: "20px",
+  },
+  footerContent: {
+    borderTop: "2px solid var(--border-default-grey)",
+    marginBottom: fr.spacing("3w"),
+  },
+  wysiwig: {
+    h3: {
+      color: fr.colors.decisions.text.active.blueFrance.default,
+    },
+  },
+}));
