c723717b42a6c0212069c7b80daac8e34a730caf
SenhaoNum <s.jiang@numericite.eu>
2026-01-12 17:34:01 +0100

    Feat : journey api fetch by personas
    Feat : journey api fetch by personas, getByPersona function returns all the content in the corresponding journey of the inputed persona

--- src/server/api/routers/journeys.ts
+++ src/server/api/routers/journeys.ts
@@ -1,30 +1,60 @@
 import { z } from "zod";
-import type { Journey } from "~/payload/payload-types";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import type { Journey, Persona } from "~/payload/payload-types";
+import {
+	createTRPCRouter,
+	fetchOrReturnRealValue,
+	publicProcedure,
+} from "~/server/api/trpc";
+import type { AugmentedPracticalGuide } from "./practical-guides";
 
-export interface AugmentedPracticalGuide extends Omit<Journey, ""> {}
+export interface AugmentedJourney extends Omit<Journey, ""> {
+	persona: Persona;
+	chapter: Chapter[];
+}
+
+interface Chapter {
+	"chapter-name": string;
+	"practical-guides": AugmentedPracticalGuide[];
+	id?: string;
+}
 
 export const journeyRouter = createTRPCRouter({
-	getByPersonaAndCondition: publicProcedure
-		.input(z.object({ persona: z.string(), condition: z.string() }))
+	getByPersona: publicProcedure
+		.input(z.object({ persona: z.string() }))
 		.query(async ({ input, ctx }) => {
 			const result = await ctx.payload.find({
 				collection: "journeys",
 				limit: 0,
-				depth: 1,
-				select: {
-					updatedAt: false,
-					createdAt: false,
-				},
+				depth: 2,
 				where: {
-					"persona.condition.slug": {
-						equals: input.condition,
-					},
-					"persona.persona.slug": {
+					"persona.slug": {
 						equals: input.persona,
 					},
 				},
 			});
-			return result.docs;
+
+			const sanitizedResult = (await Promise.all(
+				result.docs.map(async (journey) => {
+					return {
+						...journey,
+
+						persona: await fetchOrReturnRealValue(journey.persona, "personas"),
+						chapter: await Promise.all(
+							journey.chapter.map(async (chap) => ({
+								...chap,
+
+								"practical-guides": await Promise.all(
+									chap["practical-guides"].map(
+										async (pg) =>
+											await fetchOrReturnRealValue(pg, "practical-guides"),
+									),
+								),
+							})),
+						),
+					};
+				}),
+			)) as AugmentedJourney[];
+
+			return sanitizedResult;
 		}),
 });
