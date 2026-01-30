488c6045ca6adb4c5b599d8870b0f6c6698489b5
SenhaoNum <ben659@hotmail.com>
2026-01-30 17:02:44 +0100

    Feat : added informative pages accessed from footer
    Feat : added informative pages accessed from footer

---
+++ src/pages/cgu.tsx
@@ -0,0 +1,30 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import Head from "next/head";
+import { EmptyScreenZone } from "~/components/ui/EmptyScreenZone";
+import { Loader } from "~/components/ui/Loader";
+import WysiwygContent from "~/components/ui/PracticalGuides/WysiwigContent";
+import { api } from "~/utils/api";
+
+export default function Cgu() {
+	const { data: pageContent, isLoading: isLoadingData } =
+		api.cms.cgu.useQuery();
+
+	if (isLoadingData) return <Loader />;
+
+	if (!pageContent) return <EmptyScreenZone>Missing content</EmptyScreenZone>;
+
+	return (
+		<>
+			<Head>
+				<title>DITND - Politique de confidentialité</title>
+				<meta
+					name="description"
+					content={`DITND - Politique de confidentialité`}
+				/>
+			</Head>
+			<div className={fr.cx("fr-container", "fr-py-10w")}>
+				<WysiwygContent title={pageContent.title} html={pageContent.html} />
+			</div>
+		</>
+	);
+}
---
+++ src/pages/termsOfUse.tsx
@@ -0,0 +1,27 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import Head from "next/head";
+import { EmptyScreenZone } from "~/components/ui/EmptyScreenZone";
+import { Loader } from "~/components/ui/Loader";
+import WysiwygContent from "~/components/ui/PracticalGuides/WysiwigContent";
+import { api } from "~/utils/api";
+
+export default function TermsOfUse() {
+	const { data: pageContent, isLoading: isLoadingData } =
+		api.cms.termsOfUse.useQuery();
+
+	if (isLoadingData) return <Loader />;
+
+	if (!pageContent) return <EmptyScreenZone>Missing content</EmptyScreenZone>;
+
+	return (
+		<>
+			<Head>
+				<title>DITND - Modalités d'utilisation</title>
+				<meta name="description" content={`DITND - Modalités d'utilisation`} />
+			</Head>
+			<div className={fr.cx("fr-container", "fr-py-10w")}>
+				<WysiwygContent title={pageContent.title} html={pageContent.html} />
+			</div>
+		</>
+	);
+}
