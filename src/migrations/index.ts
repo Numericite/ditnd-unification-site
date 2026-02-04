9c3dc4974f3024fc7bd902f21693f6a719ae6482
SenhaoNum <ben659@hotmail.com>
2026-02-04 11:13:22 +0100

    Updated migration
    Updated migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260128_083247_init from './20260128_083247_init';
 import * as migration_20260204_095236 from './20260204_095236';
+import * as migration_20260204_101206 from './20260204_101206';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260204_095236.up,
     down: migration_20260204_095236.down,
-    name: '20260204_095236'
+    name: '20260204_095236',
+  },
+  {
+    up: migration_20260204_101206.up,
+    down: migration_20260204_101206.down,
+    name: '20260204_101206'
   },
 ];
