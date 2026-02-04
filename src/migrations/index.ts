225f65edf233f554c1d268e54427637c02212a09
SenhaoNum <ben659@hotmail.com>
2026-02-04 17:00:05 +0100

    Feat : created migration and added pictograms in seed
    Feat : created migration and added pictograms in seed

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260128_083247_init from './20260128_083247_init';
 import * as migration_20260204_095236 from './20260204_095236';
 import * as migration_20260204_101206 from './20260204_101206';
+import * as migration_20260204_152940 from './20260204_152940';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260204_101206.up,
     down: migration_20260204_101206.down,
-    name: '20260204_101206'
+    name: '20260204_101206',
+  },
+  {
+    up: migration_20260204_152940.up,
+    down: migration_20260204_152940.down,
+    name: '20260204_152940'
   },
 ];
