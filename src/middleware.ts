c22d1e28fe8e5ad14026f13f63af809fae19b796
Antoine Lelong <azenoxe@gmail.com>
2026-02-05 14:24:45 +0100

    feat: add temporary middleware www-authenticate with user + password for preprod environnement

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+// Temporary middleware for HTTP Basic Authentication in preprod environment.
+import { type NextRequest, NextResponse } from "next/server";
+
+const BASIC_USER = process.env.BASIC_USER ?? "";
+const BASIC_PASSWORD = process.env.BASIC_PASSWORD ?? "";
+
+if (!BASIC_USER || !BASIC_PASSWORD) {
+	throw new Error("Missing env.BASIC_USER or env.BASIC_PASSWORD");
+}
+
+export const isAuthorized = (authHeader: string | null): boolean => {
+	if (!authHeader || !authHeader?.startsWith("Basic ")) {
+		return false;
+	}
+	const decoded = Buffer.from(authHeader.slice(6).trim(), "base64").toString(
+		"utf8",
+	);
+	const [user, pass] = decoded.split(":");
+	return user === BASIC_USER && pass === BASIC_PASSWORD;
+};
+
+export function middleware(req: NextRequest) {
+	const authHeader = req.headers.get("authorization");
+
+	if (isAuthorized(authHeader)) return NextResponse.next();
+
+	return new Response("Auth Required", {
+		status: 401,
+		headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+	});
+}
+
+export const config = {
+	matcher: "/((?!_next).*)",
+};
