830063f8a89fa574ad10834f3fcbcaa3676ec214
SenhaoNum <ben659@hotmail.com>
2026-02-06 12:08:52 +0100

    Feat : Added migration
    Feat : Added migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260128_083247_init from './20260128_083247_init';
 import * as migration_20260204_095236 from './20260204_095236';
 import * as migration_20260204_101206 from './20260204_101206';
 import * as migration_20260204_152940 from './20260204_152940';
+import * as migration_20260206_105518 from './20260206_105518';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20260204_152940.up,
     down: migration_20260204_152940.down,
-    name: '20260204_152940'
+    name: '20260204_152940',
+  },
+  {
+    up: migration_20260206_105518.up,
+    down: migration_20260206_105518.down,
+    name: '20260206_105518'
   },
 ];
