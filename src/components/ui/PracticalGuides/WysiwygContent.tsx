32c1e0708da21feb8bbe319b4c41d319c179b95b
SenhaoNum <ben659@hotmail.com>
2026-02-11 15:08:46 +0100

    Refact : isolated converter behavior
    Refact : isolated converter behavior for cleaner code

--- src/components/ui/PracticalGuides/WysiwygContent.tsx
+++ src/components/ui/PracticalGuides/WysiwygContent.tsx
@@ -1,20 +1,16 @@
 import { tss } from "tss-react";
 import { fr } from "@codegouvfr/react-dsfr";
-import { slugify } from "~/utils/tools";
-import CardDisplay from "../Cards/CardDisplay";
+import { type Link, generateSummaryFromRichText } from "~/utils/tools";
 import {
 	defaultJSXConverters,
 	RichText,
 } from "@payloadcms/richtext-lexical/react";
-import type {
-	DefaultTypedEditorState,
-	SerializedHeadingNode,
-	SerializedRelationshipNode,
-	SerializedUploadNode,
-} from "@payloadcms/richtext-lexical";
-import type { AugmentedPracticalGuide } from "~/server/api/routers/practical-guides";
-import type { AugmentedCourse } from "~/server/api/routers/courses";
-import type { Media } from "~/payload/payload-types";
+import type { DefaultTypedEditorState } from "@payloadcms/richtext-lexical";
+import {
+	headingConverter,
+	relationshipConverter,
+	uploadConverter,
+} from "~/utils/converters";
 
 export default function WysiwygContent({
 	title,
@@ -32,89 +28,9 @@ export default function WysiwygContent({
 				data={content}
 				converters={{
 					...defaultJSXConverters,
-					heading: (args) => {
-						const { node } = args;
-
-						const headingNode = node as SerializedHeadingNode;
-
-						if (headingNode.tag === "h2") {
-							const childrenJSX = args.nodesToJSX({
-								nodes: headingNode.children ?? [],
-								converters: args.converters,
-							});
-
-							const headingText = (headingNode.children ?? [])
-								.map((child: any) => child.text ?? "")
-								.join(" ");
-
-							return <h2 id={slugify(headingText)}>{childrenJSX}</h2>;
-						}
-						const defaultHeading = defaultJSXConverters.heading;
-
-						return typeof defaultHeading === "function"
-							? defaultHeading(args)
-							: null;
-					},
-					relationship: ({ node }) => {
-						const relationshipNode = node as SerializedRelationshipNode;
-
-						if (relationshipNode.relationTo === "practical-guides") {
-							const value = relationshipNode.value as AugmentedPracticalGuide;
-
-							return (
-								<div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-									<CardDisplay
-										title={value.title}
-										conditions={value.conditions ?? []}
-										themes={value.themes}
-										redirect={`/guides/${value.slug}`}
-										titleAs="h3"
-										noImg
-									/>
-								</div>
-							);
-						}
-
-						if (relationshipNode.relationTo === "courses") {
-							const value = relationshipNode.value as AugmentedCourse;
-
-							return (
-								<div className={fr.cx("fr-col-sm-12", "fr-col-md-6")}>
-									<CardDisplay
-										title={value.title}
-										conditions={[value.condition]}
-										themes={[value.theme]}
-										redirect={value.link}
-										titleAs="h3"
-										noImg
-										kind="courses"
-									/>
-								</div>
-							);
-						}
-
-						return null;
-					},
-					upload: ({ node }) => {
-						const uploadNode = node as SerializedUploadNode;
-
-						const value = uploadNode.value as Media;
-
-						if (!value?.url) return null;
-						return (
-							<div
-								style={{ display: "flex", justifyContent: `${node.format}` }}
-							>
-								<img
-									fetchPriority="high"
-									src={`${value.url}`}
-									alt={`${value.alt || ""}`}
-									width={`${value.width || ""}`}
-									height={`${value.height || ""}`}
-								/>
-							</div>
-						);
-					},
+					heading: headingConverter,
+					relationship: relationshipConverter,
+					upload: uploadConverter,
 				}}
 			/>
 		</div>
