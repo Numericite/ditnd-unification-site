833fd47d1cb4f82856915bbcf39abb17604fbb73
SenhaoNum <ben659@hotmail.com>
2026-02-24 11:27:20 +0100

    Feat : added createAt and updatedAt on practical guides
    Feat : added createAt and updatedAt on practical guides

--- src/components/ui/PracticalGuides/WysiwygContent.tsx
+++ src/components/ui/PracticalGuides/WysiwygContent.tsx
@@ -1,19 +1,26 @@
 import { tss } from "tss-react/dsfr";
 import { fr } from "@codegouvfr/react-dsfr";
+import moment from "moment";
 import { type Link, generateSummaryFromRichText } from "~/utils/tools";
 import type { DefaultTypedEditorState } from "@payloadcms/richtext-lexical";
 import { useEffect, type Dispatch, type SetStateAction } from "react";
 import { RichTextRenderer } from "./RichTextRenderer";
 
+type Props = {
+	title: string;
+	content: DefaultTypedEditorState;
+	setMenuLinks?: Dispatch<SetStateAction<Link[]>>;
+	createdAt: string;
+	updatedAt: string;
+};
+
 export default function WysiwygContent({
 	title,
 	content,
 	setMenuLinks,
-}: {
-	title: string;
-	content: DefaultTypedEditorState;
-	setMenuLinks?: Dispatch<SetStateAction<Link[]>>;
-}) {
+	createdAt,
+	updatedAt,
+}: Props) {
 	const { classes, cx } = useStyles();
 
 	useEffect(() => {
@@ -22,15 +29,25 @@ export default function WysiwygContent({
 		setMenuLinks(generateSummaryFromRichText(content));
 	}, [content, setMenuLinks]);
 
+	const createdAtFormatted = moment(createdAt).format("DD/MM/YYYY");
+
+	const updatedAtFormatted = moment(updatedAt).format("DD/MM/YYYY");
+
 	return (
 		<div className={cx(classes.wysiwig)} id="wysiwig-content">
-			<h1>{title}</h1>
+			<h1 className={classes.wysiwygTitle}>{title}</h1>
+			<p
+				className={fr.cx("fr-text--sm")}
+			>{`Publié le ${createdAtFormatted} - Modifié le ${updatedAtFormatted}`}</p>
 			<RichTextRenderer content={content} />
 		</div>
 	);
 }
 
 const useStyles = tss.withName(WysiwygContent.name).create(() => ({
+	wysiwygTitle: {
+		marginBottom: fr.spacing("2v"),
+	},
 	wysiwig: {
 		h1: {
 			color: fr.colors.decisions.text.active.blueFrance.default,
