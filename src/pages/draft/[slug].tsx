c7f38ca30189321ee391bcfa660333d805cbbfec
SenhaoNum <ben659@hotmail.com>
2026-03-06 17:22:52 +0100

    Update [slug].tsx

--- src/pages/draft/[slug].tsx
+++ src/pages/draft/[slug].tsx
@@ -39,6 +39,7 @@ export default function PracticalGuidePage() {
 					content={`Brouillon Fiche pratique sur ${guide.title} `}
 				/>
 			</Head>
+			<h1 className={fr.cx("fr-h1")}>{guide.title}</h1>
 			<div className={fr.cx("fr-container")}>
 				<Breadcrumb
 					currentPageLabel={guide.title}
