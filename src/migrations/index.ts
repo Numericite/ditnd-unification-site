38099a0b0a14b9c9e0d38e49ada5be79b1dedee8
Clément <clement@numericite.eu>
2026-03-10 12:14:38 +0100

    fix: migration for search results

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -7,6 +7,7 @@ import * as migration_20260213_093527 from './20260213_093527';
 import * as migration_20260220_160018 from './20260220_160018';
 import * as migration_20260303_135634 from './20260303_135634';
 import * as migration_20260309_140436 from './20260309_140436';
+import * as migration_20260310_111338 from './20260310_111338';
 
 export const migrations = [
   {
@@ -52,6 +53,11 @@ export const migrations = [
   {
     up: migration_20260309_140436.up,
     down: migration_20260309_140436.down,
-    name: '20260309_140436'
+    name: '20260309_140436',
+  },
+  {
+    up: migration_20260310_111338.up,
+    down: migration_20260310_111338.down,
+    name: '20260310_111338'
   },
 ];
