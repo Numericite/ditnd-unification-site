840073d8a17f1678731451d472383437cea4dc17
Clément Lelong <clement@numericite.eu>
2026-03-23 12:22:41 +0100

    fix: add vector extension to payload schema

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -100,6 +100,7 @@ export default buildConfig({
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: postgresAdapter({
+    extensions: ["vector"],
     beforeSchemaInit: [addPracticalGuidesTable],
     afterSchemaInit: [addPracticalGuidesTableVector],
     pool: {
