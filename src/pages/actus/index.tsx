21446a8e23dfe92de8818dcf452838528a53e729
SenhaoNum <ben659@hotmail.com>
2026-04-10 16:10:50 +0200

    Feat : added WIP meta message on actualité & annuaire
    Feat : added WIP meta message on actualité & annuaire

--- src/pages/actus/index.tsx
+++ src/pages/actus/index.tsx
@@ -1,5 +1,16 @@
 import { EmptyScreenZone } from "~/components/ui/EmptyScreenZone";
+import Head from "next/head";
+
 
 export default function Actus() {
-	return <EmptyScreenZone>Actualités</EmptyScreenZone>;
+	return (
+		<>
+			<Head>
+				<title>DITND - Actualités</title>
+				<meta
+					name="description"
+					content={`Page d'actualité en cours de construction`}
+				/>
+			</Head>
+			<EmptyScreenZone>Actualités</EmptyScreenZone></>);
 }
