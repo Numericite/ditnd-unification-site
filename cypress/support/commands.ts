0a1d32c12c4effb1cf7b6f94e4be90e58faa1dcd
linnaa <linna.azaiez@gmail.com>
2026-09-10 12:10:38 +0200

    feat(cypress): pre-seed localStorage with a fuly-accepted consent state

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,30 @@
 export {};
+
+const CONSENT_STORAGE_KEY =
+	"@codegouvfr/react-dsfr finalityConsent youtube-cartographie-matomo";
+
+Cypress.Commands.overwrite(
+	"visit",
+	(originalFn, url: string, options: Partial<Cypress.VisitOptions> = {}) => {
+		const visit = originalFn as unknown as (
+			url: string,
+			options: Partial<Cypress.VisitOptions>,
+		) => Cypress.Chainable;
+
+		return visit(url, {
+			...options,
+			onBeforeLoad(win) {
+				win.localStorage.setItem(
+					CONSENT_STORAGE_KEY,
+					JSON.stringify({
+						youtube: true,
+						cartographie: true,
+						matomo: true,
+						isFullConsent: true,
+					}),
+				);
+				options.onBeforeLoad?.(win);
+			},
+		});
+	},
+);
